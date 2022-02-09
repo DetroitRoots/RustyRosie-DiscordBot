7ad509f6530c420b153c1fce6e75195646b61c63
Cyber9unk <92269361+cyber9unk@users.noreply.github.com>
2022-02-10 00:56:03 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ extern crate serenity;
 
 use serenity::prelude::*;
 use serenity::model::gateway::Ready;
+use serenity::model::channel::Message;
                   
 const TOKEN : &str = "";
-      //ボットを作成した後、Discord開発者ページからトークンを取得する必要があります。
+       //If you want to use this bot, you just need to add your token and compile it, please read the documentation for details.
 struct Handler;
 
 impl EventHandler for Handler 
 {
+     fn message(&self, ctx: Context, msg: Message)
+    {
+        if msg.content == "?ping"
+        {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?")
+            {
+                println!("Error giving message: {:?}", why);
+            }
+        }
+    }
     fn ready(&self, _: Context, ready: Ready) 
     {
         println!("{} is ready", ready.user.name);
