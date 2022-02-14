28cd9b97311d9fd8b490a0967bb16341bbd67fcf
Cyber9unk <92269361+cyber9unk@users.noreply.github.com>
2022-02-14 17:23:28 +0300

    Update
    Changelog
    - New event added.

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,20 @@ struct Handler;
 
 impl EventHandler for Handler 
 {
-    fn message(&self, ctx: Context, msg: Message)
-    {
         if msg.content == "?wru"
         {
             if let Err(why) = msg.channel_id.say(&ctx.http, "This is Rosie, long ago I was an amazing robot in The Jetsons Family. Now I got Rusty and mostly forgotten. I work as an Admin Assistant in this Discord Channel. Let's hug this old rusty machine.")
             {
                 println!("Error giving message: {:?}", why);
+            }
+        }
+        {
+            if msg.content == "?ping"
+            {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "pong?")
+                {
+                    println!("Error giving message: {:?}", why);
+                }
             }
         }
     }
